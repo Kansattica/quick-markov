7fde4d52a7a977831e67f057322a42d181194bca
Grace Lovelace <grace@hypnovir.us>
2020-11-10 14:05:08 -0800

    Add copyright notice and build instructions.

--- markov.cpp
+++ markov.cpp
@@ -1,3 +1,26 @@
+/*
+	quick-markov, a cute little markov chain program
+    Copyright (C) 2020 Grace Lovelace
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+	compile with:
+	g++ -O3 --std=c++17 markov.cpp -o markov
+ */
+
+
 #include <iostream>
 #include <string>
 #include <cctype>
